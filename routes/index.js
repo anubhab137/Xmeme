b861d4a57385fc9b63a5f7f2895d24bd956e0416
Anwesha Das <anwedas546@gmail.com>
2021-02-13 19:50:27 +0530

    adding memes/id

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
-
+var ObjectId = require('mongodb').ObjectId;
 var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
@@ -47,21 +47,22 @@ router.post('/add-memes',function(req,res,next){
   res.redirect('/memes')
 });
 
-router.get('/memes/:_id', function(req , res){
-  var id = req.body.id;
-  res.send(id);
-  // mongo.connect(url,function(err,client){
-  //   if(err) return err;
-  //   else console.log("Database connected");
-  //   var database = client.db('test');
-  //   database.collection('data').findOne({"_id": objectId(id)},function(err, result){
-  //     if(err) return err;
-  //     console.log(result);
-  //     res.send(result);
-  //     client.close();
-  //     //res.render('find-meme', {thismeme : memeid});
-  //   });
-  // });
+router.get('/memes/:id', function(req , res){
+  
+  var id=req.params.id;
+  
+  mongo.connect(url,function(err,client){
+    if(err) return err;
+    else console.log("Database connected");
+    var database = client.db('test');
+    database.collection('data').findOne({"_id": ObjectId(id)},function(err, result){
+      if(err)return err;
+      if(result==null) res.status(404).json("meme not found");
+      else res.render('find-meme', {thismeme : result});
+      client.close();
+      
+    });
+  });
 });
 
 
