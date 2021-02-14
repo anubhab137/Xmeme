83665bf66615f0421572992218df8ee1ae2662bf
Anwesha Das <anwedas546@gmail.com>
2021-02-14 13:28:24 +0530

    adding scripts

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install -y node
+sudo apt-get install -y npm
+sudo apt-get install -y nodemon
+sudo apt-get install -y express
+sudo apt-get install -y mongodb
+sudo apt-get install -y pug
+sudo apt-get install -y debug
+sudo apt-get install -y cookie-parser
+sudo apt-get install -y morgan
+sudo apt-get install -y http-errors
+
+
