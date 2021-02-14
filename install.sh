032d9d4152e4c281a8868a521de0afe6100c00be
anubhab137 <adamsanubhab@gmail.com>
2021-02-14 15:56:33 +0530

    Mongo installation added

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 sudo apt-get update
 sudo apt-get install -y node
 sudo apt-get install -y npm
@@ -9,5 +13,6 @@ sudo apt-get install -y debug
 sudo apt-get install -y cookie-parser
 sudo apt-get install -y morgan
 sudo apt-get install -y http-errors
+
 
 
